351b74ce202fc6cd1069a6a09cc57faa411a8d2d
Shubham_Joshi_ <subham.joshi@qualyval.com>
2022-01-22 16:47:11 +0530

    Completed Inplay Matches Table

--- src/MainBody.js
+++ src/MainBody.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./MainBody.css";
 
-import { DownOutlined , UpOutlined } from '@ant-design/icons';
+
+import { DownOutlined , UpOutlined, FundProjectionScreenOutlined } from '@ant-design/icons';
 
 
 
@@ -71,8 +72,316 @@ Match Amount
     <a href="#">Link 2</a>
     <a href="#">Link 3</a>
   </div>
+
 </div>
       </div>
+      <table id="customers">
+  <tr>
+    <th id="upcoming">Upcoming</th>
+    <th id="matched">Matched</th>
+    <th id="matchTableone">1</th>
+    <th>X</th>
+    <th>2</th>
+  </tr>
+  <tr>
+    <td id="upcoming">Dec 25, India vs Pakistan</td>
+    <td className="matchedData">£1,865</td>
+    <td>
+<div>
+      <div id="matchTableOneData1">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      <div id="matchTableOneData2">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      </div>
+    </td>
+    <td>
+    <div>
+      <div id="matchTableOneData1">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      <div id="matchTableOneData2">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      </div>
+    </td>
+    <td>
+    <div>
+      <div id="matchTableOneData1">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      <div id="matchTableOneData2">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      </div>
+    </td>
+  </tr>
+  <tr>
+    <td id="upcoming">Dec 28</td>
+    <td className="matchedData">£1,775</td>
+    <td>
+<div>
+      <div id="matchTableOneData1">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      <div id="matchTableOneData2">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      </div>
+    </td>
+    <td>
+    <div>
+      <div id="matchTableOneData1">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      <div id="matchTableOneData2">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      </div>
+    </td>
+    <td>
+    <div>
+      <div id="matchTableOneData1">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      <div id="matchTableOneData2">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      </div>
+    </td>
+  </tr>
+  <tr>
+    <td id="upcoming">Dec 30</td>
+    <td className="matchedData">£1,482</td>
+    <td>
+<div>
+      <div id="matchTableOneData1">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      <div id="matchTableOneData2">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      </div>
+    </td>
+    <td>
+    <div>
+      <div id="matchTableOneData1">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      <div id="matchTableOneData2">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      </div>
+    </td>
+    <td>
+    <div>
+      <div id="matchTableOneData1">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      <div id="matchTableOneData2">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      </div>
+    </td>
+  </tr>
+ 
+</table>
+<div>
+<div id="cricketThirdDiv">
+        <UpOutlined id="upArrow" />
+        <p>Test Matches</p>
+        <div className="dropdownSecond">
+        Matches
+          <DownOutlined className="dropbtn2"/>
+  <div class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+
+</div>
+      </div>
+      <table id="customers">
+  <tr>
+    <th id="upcoming">Inplay</th>
+    <th id="matched">Matched</th>
+    <th id="matchTableone">1</th>
+    <th>X</th>
+    <th>2</th>
+  </tr>
+  <tr>
+    <td>
+
+    <div id="inPlayTop"> 
+<p>1.46</p>
+<p>£90</p>
+
+    </div>
+
+    <p id="inPlayTeam">Perth Scorchers
+Hobart Hurricanes</p>
+  
+    </td>
+    <td className="matchedData">
+    £1,775
+    <FundProjectionScreenOutlined id="tvIcons" />
+    </td>
+    <td>
+<div>
+      <div id="matchTableOneData1">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      <div id="matchTableOneData2">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      </div>
+    </td>
+    <td>
+    <div>
+      <div id="matchTableOneData1">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      <div id="matchTableOneData2">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      </div>
+    </td>
+    <td>
+    <div>
+      <div id="matchTableOneData1">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      <div id="matchTableOneData2">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      </div>
+    </td>
+  </tr>
+
+
+</table>
+</div>
+  <table id="customers">
+  <tr>
+    <th id="upcoming">Upcoming</th>
+    <th id="matched">Matched</th>
+    <th id="matchTableone">1</th>
+    <th>X</th>
+    <th>2</th>
+  </tr>
+  <tr>
+    <td id="upcoming">Dec 25, India vs Pakistan</td>
+    <td className="matchedData">£1,865</td>
+    <td>
+<div>
+      <div id="matchTableOneData1">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      <div id="matchTableOneData2">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      </div>
+    </td>
+    <td>
+    <div>
+      <div id="matchTableOneData1">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      <div id="matchTableOneData2">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      </div>
+    </td>
+    <td>
+    <div>
+      <div id="matchTableOneData1">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      <div id="matchTableOneData2">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      </div>
+    </td>
+  </tr>
+  <tr>
+    <td id="upcoming">Dec 28</td>
+    <td className="matchedData">£1,775</td>
+    <td>
+<div>
+      <div id="matchTableOneData1">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      <div id="matchTableOneData2">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      </div>
+    </td>
+    <td>
+    <div>
+      <div id="matchTableOneData1">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      <div id="matchTableOneData2">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      </div>
+    </td>
+    <td>
+    <div>
+      <div id="matchTableOneData1">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      <div id="matchTableOneData2">
+        <p>6</p>
+        <p>£90</p>
+      </div>
+      </div>
+    </td>
+  </tr>
+  <tr>
+    
+    
+    
+  </tr>
+ 
+</table>
+
     </div>
   );
 };
