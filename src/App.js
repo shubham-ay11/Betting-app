dceb051e9a3422e049ce7ee2becf826817147e7a
Shubham_Joshi_ <subham.joshi@qualyval.com>
2022-02-02 13:50:28 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
 <SideNav />
           </div>
            <div className="main">
-<MainBody />
-{/* <ScoreBoard /> */}
+{/* <MainBody /> */}
+<ScoreBoard />
 </div>
 </div>
 </div>
